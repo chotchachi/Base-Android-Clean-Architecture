b363246ce4321a5f229a5344e5bda2fa4a194c73
Thanh Quang <quangtt@foxcode.org>
2022-07-23 15:06:10 +0700

    [QuangTT]: - fix trigger flow

--- utils/src/main/java/com/utils/extensions/flow/triggerFlow.kt
+++ utils/src/main/java/com/utils/extensions/flow/triggerFlow.kt
@@ -7,9 +7,12 @@ import kotlinx.coroutines.flow.*
  * Created by Thanh Quang on 13/04/2022.
  */
 @FlowPreview
-fun triggerFlow(trigger: Trigger, flowProvider: (() -> Flow<Any>)? = null) =
+fun <T> triggerFlow(trigger: Trigger, flowProvider: () -> Flow<T>) =
     trigger.triggerEvent
-        .flatMapConcat { flowProvider?.invoke() ?: flowOf(Unit) }
+        .flatMapConcat { flowProvider.invoke() }
+
+fun triggerFlow(trigger: Trigger) = trigger.triggerEvent
+    .mapToUnit()
 
 class Trigger {
     val triggerEvent = MutableSharedFlow<Unit>(extraBufferCapacity = 64)
